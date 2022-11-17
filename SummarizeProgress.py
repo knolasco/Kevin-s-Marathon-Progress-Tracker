e16a2303e610a30c74802f66f62b7f33d364d5c0
Kevin Nolasco <kevnolasco95@gmail.com>
2022-11-16 20:56:00 -0800

    updating plots

--- SummarizeProgress.py
+++ SummarizeProgress.py
@@ -23,6 +23,7 @@ class ProgressDashboard:
     # initialize and open csv
     def __init__(self, DATA, GOAL_PACE):
         self.df = pd.read_csv(DATA)
+        self.df['Date'] = pd.to_datetime(self.df['Date'])
         self.goal_pace = GOAL_PACE
 
     def clean_data(self):
@@ -52,6 +53,16 @@ class ProgressDashboard:
         # update legend
         self.axs[0].set_title('Progress Miles versus Total Training Miles')
 
+
+    def plot_pie(self):
+        actual_runs = self.df[~self.df['Fastest_Mile'].isna()]['Miles_Ran'].sum()
+        final_distance = self.df['Miles_Ran'].sum()
+        remaining = final_distance - actual_runs
+        
+        data = [actual_runs, remaining]
+        self.axs[0].pie(data, labels = ['Actual Mileage Ran', 'Total Training Miles'], explode = [0.1,0],
+            colors = ['#44BBA4', '#393E41'], autopct= lambda p:f'{p*sum(data)/100 :.0f} Miles ({p:.2f}%)', radius = 1.5)
+
     # plot pace
     def plot_pace(self):
         # pivot df for graphing
@@ -66,8 +77,10 @@ class ProgressDashboard:
         g = sns.lineplot(data = self.df_m, x = 'Date', y = 'Minutes Per Mile', hue = 'Pace', ax = self.axs[1], legend = 'full', palette = palette, linewidth = 5)
         self.axs[1].plot([self.df_m['Date'].min(), self.df_m['Date'].max()], [self.goal_pace, self.goal_pace], 'k--')
         self.axs[1].text(sorted(self.df_m['Date'].unique())[-3], 6.5, 'Boston Marathon Qualifying Pace', fontsize = 12)
-        self.axs[1].set_ylim(5.5, 9)
+        self.axs[1].set_ylim(5.5, 10.5)
         self.axs[1].set_title('Mile Pace Comparison Against Daily Goals')
+        ticks = self.axs[1].get_xticks()
+        self.axs[1].set_xticks([ticks[0], ticks[int(len(ticks)/2)], ticks[-1]], visible = True)
     
     def plot_scatter(self):
         self.filtered_df['Boston_Qualifying'] = self.filtered_df['Avg_Pace_converted'].apply(lambda pace: 'Qualifying' if pace < self.goal_pace else 'Not Qualifying')
@@ -75,7 +88,7 @@ class ProgressDashboard:
                     'Not Qualifying' : '#E94F37'}
         g = sns.scatterplot(data = self.filtered_df, x = 'Miles_Ran', y = 'Avg_Pace_converted', hue = 'Boston_Qualifying', ax = self.axs[2], palette = palette, s = 100)
         self.axs[2].set_title('Comparing Miles Ran with Average Mile Pace')
-        self.axs[2].set_ylim(5.5, 9)
+        self.axs[2].set_ylim(5.5, 10.5)
 
     def plot_goal_comparison(self):
         self.filtered_df['Seconds_To_Goal'] = self.filtered_df['Avg_Pace_converted'].apply(lambda pace: (pace - self.goal_pace)*60)
@@ -84,11 +97,13 @@ class ProgressDashboard:
         sns.lineplot(data = self.filtered_df, x = 'Date', y = 'Seconds_To_Goal', ax = self.axs[3], color = '#393E41', linewidth = 3)
         sns.scatterplot(data = self.filtered_df, x = 'Date', y = 'Seconds_To_Goal', hue = 'Boston_Qualifying', ax = self.axs[3], palette = palette, s = 200)
         self.axs[3].set_title('Seconds per Mile to Qualify For Boston Marathon')
-        self.axs[3].set_ylim(-120,120)
+        self.axs[3].set_ylim(-360,360)
+        ticks = self.axs[3].get_xticks()
+        self.axs[3].set_xticks([ticks[0], ticks[int(len(ticks)/2)], ticks[-1]], visible = True)
 
 
     def plot(self):
-        self.plot_actual()
+        self.plot_pie()
         self.plot_pace()
         self.plot_scatter()
         self.plot_goal_comparison()
